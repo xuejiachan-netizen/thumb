5e0f4159444b4c9e9332b1da31c6e21424da19c4
chenxuanjia <chenxuanjia@>
2026-08-18 10:58:41 +0800

    test

--- src/main/java/com/xuanjia/millionlikes/MIllionLikesApplication.java
+++ src/main/java/com/xuanjia/millionlikes/MIllionLikesApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class MIllionLikesApplication {
-
+//
     public static void main(String[] args) {
         SpringApplication.run(MIllionLikesApplication.class, args);
     }
